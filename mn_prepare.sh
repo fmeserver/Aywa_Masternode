df461fa46313f963a1e2dc7248b5ed0f04ae9e0f
NiconX <Nicon.Xenakis@gmail.com>
2018-11-18 11:45:09 +0300

    modified:   mn_prepare.sh

--- mn_prepare.sh
+++ mn_prepare.sh
@@ -147,10 +147,11 @@ for i in `seq 1 $MN_COUNT`;
 
 
 ##### Main #####
+
 clear
 add_user
 add_swap
 install_dependencies
 su $NEWUSERNAME -c "$(declare -f download_aywacore); download_aywacore"
-echo "No requirements to use root or sudo for Aywa MN management. Use account $NEWUSERNAME for ssh logon."
-echo 'MN Server needs to Reboot to continue MN installation? Are you ready(y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot
+echo "You dot't need to use root and sudo for Aywa MN management. Logon ssh again with user: $NEWUSERNAME"
+echo 'MN Server need to Reboot to continue MN installation? Are you ready(y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot
