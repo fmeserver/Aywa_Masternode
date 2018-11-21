df0245fcbe3674eaa32f0b37248629fc8bcfa289
Aywa Developers <44238474+GetAywa@users.noreply.github.com>
2018-11-21 13:50:38 +0300

    Update mn_prepare.sh

--- mn_prepare.sh
+++ mn_prepare.sh
@@ -152,7 +152,8 @@ for i in `seq 1 $MN_COUNT`;
 clear
 add_user
 add_swap
-install_dependencies
-su $NEWUSERNAME -c "$(declare -f download_aywacore); download_aywacore"
+#install_dependencies
+#su $NEWUSERNAME -c "$(declare -f download_aywacore); download_aywacore"
+echo "$1 $2 $3"
 echo "You dot't need to use root and sudo for Aywa MN management. Logon ssh again with user: $NEWUSERNAME"
 echo 'MN Server need to Reboot to continue MN installation? Are you ready(y/n)' && read x && [[ "$x" == "y" ]] && /sbin/reboot
